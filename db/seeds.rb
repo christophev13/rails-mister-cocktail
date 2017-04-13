d1aa61d7ddd671d33376ec9fdb66a0284b78a5bf
christophe vallerin <christophe.vallerin@hotmail.com>
2017-04-13 15:44:54 -0300

    Add index page and some styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+
+# seed Ingredient
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+
+# seed Cocktails
+
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "caipirinha")
+Cocktail.create(name: "jack&cola")
+Cocktail.create(name: "screw driver")
+Cocktail.create(name: "spritz")
+Cocktail.create(name: "banana daiquiri")
